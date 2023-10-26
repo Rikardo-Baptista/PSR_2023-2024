8b71bb5d7858ce805ce5c606cdb850f9d8c120ad
Rikardo-Baptista <ricardo.baptista@ua.pt>
2023-10-26 17:20:13 +0100

    update

--- Parte5/Ex3/main3d.py
+++ Parte5/Ex3/main3d.py
@@ -1,37 +1,61 @@
 #!/usr/bin/env python3
 #shebang line to inform the OS that the content is in python
 
+#Exercicio 3d:
+#Implemente um programa que permita configurar a segmentação de cor. 
+#O programa deve executar a segmentação verificando quais os pixeis 
+#da imagem que estão dentro de certos limites mínimo e máximo. 
+#Estes limites deverão ser diferentes para cada canal de cor. 
+
 import argparse
+import json
 import cv2
-import numpy as np
+from functools import partial
+
+def onTrackbar(val, image_gray, window_name):
+    _,image_thresholded = cv2.threshold(image_gray, val, 255, cv2.THRESH_BINARY)
+    cv2.imshow(window_name, image_thresholded)  # Show the image
+
+def onMouse(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print('({}, {})'.format(x,y))
 
 
 def main():
+    
+    #Declaration variables
+    window_name = 'window - Ex3c'
+    image_gray = None
+    val = 128
 
     parser = argparse.ArgumentParser(description='Script to compute perfect numbers.')
     parser.add_argument('-if', '--image_filename', type=str, help='', required=False, 
-                        default='../images/atlas2000_e_atlasmv.png')
+                        default='../images/atlascar.png')
+    args = vars(parser.parse_args())
 
-    args = vars(parser.parse_args()) # creates a dictionary
-    print(args)
-
-    image_filename = args['image_filename']
-    image_rgb = cv2.imread(image_filename, cv2.IMREAD_COLOR) # Load an image
+    image = cv2.imread(args['image_filename'], cv2.IMREAD_COLOR) # Load an image
+    cv2.imshow('Original', image) #Visualizar imagem original
     
+    image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # convert bgr to gray image (single channel)
+    
+    cv2.namedWindow(window_name)
+    #cv2.createTrackbar
+    cv2.createTrackbar('', window_name, 0, 255, 
+                       partial(onTrackbar, image_gray=image_gray, window_name=window_name))
+    #Setting position of threshold trackbar to 128
+    cv2.setTrackbarPos('Threshold', window_name, val)
+    cv2.setMouseCallback(window_name, onMouse)
+    
+    onTrackbar(val, image_gray, window_name)
+    
+    cv2.waitKey(0)
 
-    lower_bound = np.array([35, 100, 100])
-    upper_bound = np.array([85, 255, 255])
+    file_name = 'limits.json'
+    with open(file_name, 'w') as file_handle:
+        print('writing dictionary d to file ' + file_name)
+        json.dump(d, file_handle) # d is the dicionary
 
-    mask = cv2.inRange(image_rgb, lower_bound, upper_bound)
-
-    #Aplicar a máscara na imagem original para destacar a cor verde
-    image_mask = cv2.bitwise_and(image_rgb, image_rgb, mask=mask)
-
-
-
-    cv2.imshow('image_rgb', image_rgb)  # Display the image
-    cv2.imshow("image_mask.png", image_mask)
-    cv2.waitKey(0) # wait for a key press before proceeding
+    # FaLtA alterar CODIGO
 
 
 if __name__ == '__main__':
