5a9460320d8ff29390dfda623c5ced5511f76815
Rikardo-Baptista <ricardo.baptista@ua.pt>
2023-11-30 01:20:17 +0000

    adding parte10

--- Parte10/psr_aula10_ex5/src/publish_webcam.py
+++ Parte10/psr_aula10_ex5/src/publish_webcam.py
@@ -1,16 +1,51 @@
 #!/usr/bin/env python3
 
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
 import rospy
+import cv2
 
 def main():
-    rospy.init_node('image_pub')
-    rospy.loginfo('image_pub node started')
+    #--------------------------
+    # Initialization
+    #--------------------------
 
+    rospy.init_node('image_publisher', anonymous=False)
+
+    publisher = rospy.Publisher('~image', Image, queue_size=1)
+
+    capture = cv2.VideoCapture(0)
+    window_name = 'window'
+    cv2.namedWindow(window_name,cv2.WINDOW_AUTOSIZE)
+
+    rate = rospy.Rate(15)
+   
+    #--------------------------
+    # Execution
+    #--------------------------
+    while True:
+        _, image = capture.read()  # get an image from the camera
+
+    #--------------------------
+    # Visualization
+    #--------------------------
+        cv2.imshow('window', image)  # Display the image
+        
+        bridge = CvBridge()
+        image_message = bridge.cv2_to_imgmsg(image, encoding='bgr8')
+        publisher.publish(image_message)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+        rate.sleep()
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+    rospy.spin()
+    
 if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        pass
-
+    main()
 
     
